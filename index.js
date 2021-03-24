4884b1b098a168f57d3ddbcd46eda91b5e836526
Vadym <tvadost2003@gmail.com>
2021-03-24 07:45:37 +0200

    examples

--- index.js
+++ index.js
@@ -68,3 +68,39 @@ console.log("\n2. BEFORE:", util.inspect(src2, false, null, true))
 mapObjectByProto(src2, proto2)
 console.log("2. AFTER:", util.inspect(src2, false, null, true))
 console.log("2. PROTO:", util.inspect(proto2, false, null, true))
+
+
+const src3 = {
+  prop11: 11,
+  prop12: 12,
+  prop13: {
+    prop21: 21,
+    prop22: {
+      prop31: 31
+    }
+  }
+}
+
+const proto3 = {
+  prop11: {
+    prop22: {
+      prop31: null,
+      prop32: null
+    },
+  },
+  prop13: {
+    prop22: {
+      prop31: null,
+      prop32: {
+        prop42: null
+      }
+    }
+  }
+}
+
+console.log('-------------------------------------------');
+
+console.log("\n3. BEFORE:", util.inspect(src3, false, null, true))
+mapObjectByProto(src3, proto3)
+console.log("3. AFTER:", util.inspect(src3, false, null, true))
+console.log("3. PROTO:", util.inspect(proto3, false, null, true))
