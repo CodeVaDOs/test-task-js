661f7dff0f6a5aae482a80447d6abc41ec001b2a
Vadym <tvadost2003@gmail.com>
2021-04-01 12:44:32 +0300

    upload

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 const util = require('util')
 const {mapObjectByProto} = require("./task_1_3");
 
-const src1 = {
+const src = {
   prop11: {
     prop21: 21,
     prop22: {
@@ -10,97 +10,18 @@ const src1 = {
     }
   },
   prop12: 12
-}
+};
 
-const proto1 = {
+const proto = {
   prop11: {
-    prop22: null
+    prop22:null
   },
   prop12: null
-}
+};
 
-console.log("1. BEFORE:", util.inspect(src1, false, null, true))
-const result1 = mapObjectByProto(src1, proto1)
-console.log("1. AFTER:", util.inspect(result1, false, null, true))
-console.log("1. PROTO:", util.inspect(proto1, false, null, true))
+console.log("src: (перед обработкой)", util.inspect(src));
 
+const result = mapObjectByProto(src, proto);
 
-const src2 = {
-  prop11: {
-    prop21: 21,
-    prop22: {
-      prop31: 31,
-      prop32: 32
-    }
-  },
-  prop12: 12,
-  prop13: {
-    prop21: 21,
-    prop22: {
-      prop31: 31,
-      prop32: {
-        prop41: 41,
-        prop42: 42
-      }
-    }
-  }
-}
-
-const proto2 = {
-  prop11: {
-    prop22: {
-      prop32: null
-    },
-  },
-  prop13: {
-    prop22: {
-      prop31: null,
-      prop32: {
-        prop42: null
-      }
-    }
-  }
-}
-
-console.log('-------------------------------------------');
-
-console.log("\n2. BEFORE:", util.inspect(src2, false, null, true))
-const result2 = mapObjectByProto(src2, proto2)
-console.log("2. AFTER:", util.inspect(result2, false, null, true))
-console.log("2. PROTO:", util.inspect(proto2, false, null, true))
-
-
-const src3 = {
-  prop11: 11,
-  prop12: 12,
-  prop13: {
-    prop21: 21,
-    prop22: {
-      prop31: 31
-    }
-  }
-}
-
-const proto3 = {
-  prop11: {
-    prop22: {
-      prop31: null,
-      prop32: null
-    },
-  },
-  prop13: {
-    prop22: {
-      prop31: null,
-      prop32: {
-        prop42: null
-      }
-    }
-  }
-}
-
-console.log('-------------------------------------------');
-
-console.log("\n3. BEFORE:", util.inspect(src3, false, null, true))
-const result3 = mapObjectByProto(src3, proto3)
-console.log("3. AFTER:", util.inspect(result3, false, null, true))
-console.log("3. PROTO:", util.inspect(proto3, false, null, true))
+console.log("src: (после обработки)", util.inspect(src));
+console.log("result: (после обработки)", util.inspect(result));
--- js/task_1_3.js
+++ js/task_1_3.js
@@ -2,9 +2,9 @@ const mapObjectByProto = (obj, proto, _newObj = {}) => {
   for (const key in obj) {
     if (!proto) continue;
 
-    proto[key] === null || proto[key]
-      ? _newObj[key] = obj[key]
-      : delete _newObj[key];
+    if (proto[key] === null || proto[key]) {
+      _newObj[key] = typeof obj[key] === 'object' ? {...obj[key]} : obj[key];
+    } else delete _newObj[key];
 
     if (typeof _newObj[key] === 'object') mapObjectByProto(obj[key], proto[key], _newObj[key]);
   }
